3386fd3d8ff8f155afc6ab8fd6742c26c400e09d
Skiada Alexandre <25521495+askiada@users.noreply.github.com>
2023-07-14 13:01:41 +0100

    UNTESTED add sink from chan

--- pkg/pipeline/sink.go
+++ pkg/pipeline/sink.go
@@ -76,3 +76,53 @@ func AddSink[I any](p *Pipeline, name string, input *Step[I], sinkFn func(ctx co
 
 	return nil
 }
+
+func AddSinkFromChan[I any](p *Pipeline, name string, input *Step[I], stepFn func(ctx context.Context, input <-chan I) error) error {
+	if p == nil {
+		return ErrPipelineMustBeSet
+	}
+	if input == nil {
+		return ErrInputMustBeSet
+	}
+	step := Step[I]{
+		Type: sinkStepType,
+		Name: name,
+	}
+	if p.drawer != nil {
+		err := p.drawer.addStep(step.Name)
+		if err != nil {
+			return err
+		}
+		err = p.drawer.addLink(input.Name, step.Name)
+		if err != nil {
+			return err
+		}
+	}
+	if p.measure != nil {
+		mt := p.measure.addStep(step.Name, 1)
+		step.metric = mt
+
+		err := p.drawer.addLink(step.Name, "end")
+		if err != nil {
+			return err
+		}
+	}
+
+	errC := make(chan error, 1)
+	decoratedError := newErrorChan(name, errC)
+	go func() {
+		defer func() {
+			close(errC)
+		}()
+		err := stepFn(p.ctx, input.Output)
+		if err != nil {
+			errC <- err
+		}
+		if step.metric != nil {
+			step.metric.end(time.Since(p.startTime))
+		}
+	}()
+	p.errcList.add(decoratedError)
+
+	return nil
+}
